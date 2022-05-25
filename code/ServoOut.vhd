906fb518fc4b9bfe5bcafe3c2b04eedc03813e0a
Nouuu13 <100780220+Nouuu13@users.noreply.github.com>
2022-05-25 15:56:54 +0200

    Add files via upload

---
+++ code/ServoOut.vhd
@@ -0,0 +1,75 @@
+-----------------------------------------------------------------------------
+-- Title           : Title
+-----------------------------------------------------------------------------
+-- Author          : Detrait Romain
+-- Date Created    : 23/05/22
+-----------------------------------------------------------------------------
+-- Description     : Description
+--							
+--
+-----------------------------------------------------------------------------
+-- Copyright 2016. All rights reserved
+-----------------------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+
+entity ServoOut is
+	port 
+	(
+		clk		: in std_logic;
+		rst_n		: in std_logic;
+		reg1		: in std_logic_vector(7 downto 0);
+		ena_in	: in std_logic;
+		gpio0		: 	out std_logic
+		
+	);
+end entity;
+
+architecture RTL of ServoOut is
+	
+	type state_type is (s0,s1);
+	signal state : state_type; 
+	
+begin
+	
+	
+		process (clk,rst_n,ena_in,reg1)
+			variable count : natural := 0;
+			constant MIN : natural :=50000;
+			constant MAX : natural :=20e6/20;
+			constant k : natural :=196;
+			variable var: natural := 0;
+		
+	begin
+		if rst_n = '0' then
+			count := 0;
+			state <= s0;
+			gpio0 <= '1';-- Output signal for the servomotor.
+		elsif rising_edge(clk) then
+			case state is
+				when s0 =>
+					count := 0;
+					if ena_in = '1' then 
+						gpio0 <= '1';
+						state <= s1; 
+					end if;
+				when s1 =>
+					count := count + 1; 
+					if count >= MIN+k*to_integer(unsigned(reg1)) then
+						gpio0 <= '0';
+					end if;				
+					if count >= MAX then
+						count := 0;
+						gpio0 <= '1';
+						state <= s0;
+					end if;
+				when others =>
+					count :=0;
+					state <= s0;	
+			end case;					
+		end if;
+	end process;
+end RTL;
---
+++ code/ServoOut_TB.vhd
@@ -0,0 +1,106 @@
+-----------------------------------------------------------------------------
+-- Title           : Title
+-----------------------------------------------------------------------------
+-- Author          : Daniel Pelikan
+-- Date Created    : 01-07-2016
+-----------------------------------------------------------------------------
+-- Description     : Description
+--							
+--
+-----------------------------------------------------------------------------
+-- Copyright 2016. All rights reserved
+-----------------------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+
+entity ServoOut_TB is 
+end entity;
+
+architecture TB of ServoOut_TB is
+     constant clk_period : time := 20 ns;
+	component ServoOut is
+		port 
+		(
+			clk		: in std_logic;
+			rst_n		: in std_logic;
+			reg1		: in std_logic_vector(7 downto 0);
+			ena_in	: in std_logic;
+			gpio0		: out std_logic
+			
+		);
+	end component;
+ 
+
+	signal clk			: std_logic;
+	signal rst_n		: std_logic;
+	signal reg1		   : std_logic_vector(7 downto 0);
+	signal gpio0			: std_logic;			
+
+begin
+
+	UUT: ServoOut 
+		port map 
+		(
+			clk			=> clk,
+			rst_n		=> rst_n,
+			reg1		=> reg1,
+			ena_in		=> '1',
+			gpio0		=> gpio0			
+		);
+		  
+     clk_process :process
+     begin
+          CLK <= '1';
+          wait for clk_period/2;
+          CLK <= '0';
+          wait for clk_period/2;
+     end process;
+     rst_process :process
+     begin
+          rst_n <= '0';
+          wait for clk_period/2;
+          rst_n <= '1';
+		  wait;		  
+     end process;
+	 
+	 
+	 
+     stimulus : process
+     begin
+		reg1 <= "00000000";
+		wait until gpio0 = '1';
+		wait until gpio0 = '0';
+		wait until gpio0 = '1';
+		reg1 <= "00000001";
+		wait until gpio0 = '1';
+		wait until gpio0 = '0';
+		wait until gpio0 = '1';
+		reg1 <= "00000010";
+		wait until gpio0 = '1';
+		wait until gpio0 = '0';
+		wait until gpio0 = '1';
+		reg1 <= "00000011";
+		wait until gpio0 = '1';
+		wait until gpio0 = '0';
+		wait until gpio0 = '1';
+		reg1 <= "00000100";
+		wait until gpio0 = '1';
+		wait until gpio0 = '0';
+		wait until gpio0 = '1';
+		reg1 <= "00000101";
+		wait until gpio0 = '1';
+		wait until gpio0 = '0';
+		wait until gpio0 = '1';
+		reg1 <= "00001000";
+		wait until gpio0 = '1';
+		wait until gpio0 = '0';
+		wait until gpio0 = '1';
+		reg1 <= "11111111";
+		wait until gpio0 = '1';
+		wait until gpio0 = '0';
+		wait until gpio0 = '1';
+     end process;		
+end TB;
